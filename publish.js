0c3e1fcfb644b28ae729f89baff192244dbe3810
dizhi33 <hlcgwcom@gmail.com>
2025-07-26 10:56:09 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,10 +37,10 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".hkvhfsv.cc");  
-       urls.push(".ynctlnfu.cc");
-       urls.push(".rmnainq.top"); 
-       urls.push(".jsanbyy.top");  
+       urls.push(".rqmbosqc.com");  
+       urls.push(".hkvhfsv.cc");
+       urls.push(".ynctlnfu.cc"); 
+       urls.push(".rmnainq.top");  
           
       
                                                                                  
