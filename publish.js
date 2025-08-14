7d98b63cb2fd039788becf63046681abc8ab9315
dizhi33 <hlcgwcom@gmail.com>
2025-08-14 21:30:23 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,14 +37,14 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".peuhfzi.cc");  
-       urls.push(".jtnispe.cc");
-       urls.push(".cbmdirrm.org"); 
-       urls.push(".saaofhev.top");  
+       urls.push(".syhlrri.org");  
+       urls.push(".peuhfzi.cc");
+       urls.push(".jtnispe.cc"); 
+       urls.push(".cbmdirrm.org");  
           
       
                                                                                  
-var JumpPage="https://91wang102.com";
+var JumpPage="https://91wang103.com";
 
 var newestUrls = [];
 
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
