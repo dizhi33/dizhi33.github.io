6cd17c54470dd4567be3ac745dddb02cbb175b25
dizhi33 <hlcgwcom@gmail.com>
2025-09-13 16:55:44 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,10 +37,10 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".amemxcnw.xyz");  
-       urls.push(".qveemyw.com");
-       urls.push(".utixwfbw.top"); 
-       urls.push(".ikzhike.cc");  
+       urls.push(".jvrloqoa.xyz");  
+       urls.push(".amemxcnw.xyz");
+       urls.push(".qveemyw.com"); 
+       urls.push(".utixwfbw.top");  
           
       
                                                                                  
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
