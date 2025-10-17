4711ec38df3846294969012145cebe3afca24cba
dizhi33 <hlcgwcom@gmail.com>
2025-10-17 11:51:08 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,14 +37,14 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".wagapntb.cc");  
-       urls.push(".skeanrbl.xyz");
-       urls.push(".ermevzyf.com"); 
-       urls.push(".dzdkioke.xyz");  
+       urls.push(".qjisgtl.com");  
+       urls.push(".mkkmlmz.cc");
+       urls.push(".wagapntb.cc"); 
+       urls.push(".skeanrbl.xyz");  
           
       
                                                                                  
-var JumpPage="https://91porna3.com";
+var JumpPage="https://91porna4.com";
 
 var newestUrls = [];
 
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
