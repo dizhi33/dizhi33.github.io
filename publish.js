23f139f32dc7ad8dc8fc0769448d639c8517e26b
dizhi33 <hlcgwcom@gmail.com>
2025-11-12 21:54:35 +0700

    Refactor subdomain and URL generation logic

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".qeasvjaj.cc");  
+       urls.push(".yhhfdtm.cc");  
        urls.push(".hsjcxgu.xyz");
        urls.push(".xbmjoxlc.xyz"); 
        urls.push(".uxdxuxul.cc");  
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
