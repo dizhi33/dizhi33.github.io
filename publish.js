3df05b09479f4a63063d0e3805e21da72ea91060
dizhi33 <hlcgwcom@gmail.com>
2025-11-21 10:25:45 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,14 +37,14 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".rhwqoqc.xyz");  
-       urls.push(".ipkuybvp.xyz");
-       urls.push(".gwhcndaw.cc"); 
-       urls.push(".xosemgua.xyz");  
+       urls.push(".shmpgiya.cc");  
+       urls.push(".rhwqoqc.xyz");
+       urls.push(".ipkuybvp.xyz"); 
+       urls.push(".gwhcndaw.cc");  
           
       
                                                                                  
-var JumpPage="https://91porna19com.com";
+var JumpPage="https://91porna10.com";
 
 var newestUrls = [];
 
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
