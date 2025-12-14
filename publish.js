7b0e898361bfc7d70b10956393fbdafb2bb42121
dizhi33 <hlcgwcom@gmail.com>
2025-12-14 20:10:26 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,10 +37,10 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".ghmvcco.cc");  
-       urls.push(".uwketik.com");
-       urls.push(".fddptkgv.cc"); 
-       urls.push(".tyjwlurx.cc");  
+       urls.push(".nsoxsrnj.com");  
+       urls.push(".ghmvcco.cc");
+       urls.push(".uwketik.com"); 
+       urls.push(".fddptkgv.cc");  
           
       
                                                                                  
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
