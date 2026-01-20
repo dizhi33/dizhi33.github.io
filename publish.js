14d2a049e0321a5ae295ef4e4d3622596f962a3d
dizhi33 <hlcgwcom@gmail.com>
2026-01-20 22:16:45 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,10 +37,10 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".xmmddfh.cc");  
-       urls.push(".yvzhhwdl.cc");
-       urls.push(".mjczamyk.cc"); 
-       urls.push(".mjczamyk.cc");  
+       urls.push(".lcrmpmh.cc");  
+       urls.push(".unlmqew.cc");
+       urls.push(".xmmddfh.cc"); 
+       urls.push(".yvzhhwdl.cc");  
           
       
                                                                                  
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
