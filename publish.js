adbfa727657547a8ccc36e4cd466e6b4e13200bd
dizhi33 <hlcgwcom@gmail.com>
2026-01-23 21:59:05 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".teezqhts.cc");  
+       urls.push(".gjpnsmg.xyz");  
        urls.push(".sbkjzutx.xyz");
        urls.push(".teezqhts.cc"); 
        urls.push(".ilpcosog.com");  
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
