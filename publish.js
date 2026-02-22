5d16822a443e89d99ba80a6538409ecf42c91a91
dizhi33 <hlcgwcom@gmail.com>
2026-02-22 21:06:18 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -39,8 +39,8 @@ var emails = [
 var urls=[]; 
        urls.push(".ugruyjoo.xyz");  
        urls.push(".mqguslvj.com");
-       urls.push(".mcsmitf.cc"); 
-       urls.push(".xytsvymz.cc");  
+       urls.push(".cyyahvav.cc"); 
+       urls.push(".vnxezpe.com");  
           
       
                                                                                  
@@ -166,6 +166,7 @@ window.onload = function () {
     var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
